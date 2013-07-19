ee281ffc25576d1006c4037e69b76e8c24d74e4b
erik <erikness@nmsu.edu>
2013-07-18 21:22:48 -0600

    Updated Tester to use a Root instead of a square root.

--- src/com/erikleeness/graph/expression/Tester.java
+++ src/com/erikleeness/graph/expression/Tester.java
@@ -4,7 +4,7 @@ import com.erikleeness.graph.expression.functions.Constant;
 import com.erikleeness.graph.expression.functions.Cosine;
 import com.erikleeness.graph.expression.functions.Exponent;
 import com.erikleeness.graph.expression.functions.Product;
-import com.erikleeness.graph.expression.functions.SquareRoot;
+import com.erikleeness.graph.expression.functions.Root;
 import com.erikleeness.graph.expression.functions.Tangent;
 import com.erikleeness.graph.expression.functions.Term;
 import com.erikleeness.graph.expression.functions.Variable;
@@ -14,7 +14,7 @@ public class Tester
 	public static void main(String[] args)
 	{
 		Term exponentPart = new Exponent(new Constant(Math.E), 
-				new Cosine(new Product(new Constant(5), new SquareRoot(new Variable()))));
+				new Cosine(new Product(new Constant(5), new Root(new Constant(2), new Variable()))));
 		Term t1 = new Product( new Constant(2), new Product( new Tangent(new Variable()), exponentPart));
 		
 		System.out.println(t1);
